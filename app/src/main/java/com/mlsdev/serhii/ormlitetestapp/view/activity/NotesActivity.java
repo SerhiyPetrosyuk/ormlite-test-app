90494901553fccabbcb26e288f4b8680039a7c03
Serhii Petrosiuk <petrosyuk@mlsdev.com>
2016-04-05 13:58:02 +0300

    Enabled the data binding

--- app/src/main/java/com/mlsdev/serhii/ormlitetestapp/view/activity/NotesActivity.java
+++ app/src/main/java/com/mlsdev/serhii/ormlitetestapp/view/activity/NotesActivity.java
@@ -1,15 +1,18 @@
 package com.mlsdev.serhii.ormlitetestapp.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mlsdev.serhii.ormlitetestapp.R;
+import com.mlsdev.serhii.ormlitetestapp.databinding.ActivityNotesBinding;
 
 public class NotesActivity extends AppCompatActivity {
+    private ActivityNotesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_notes);
     }
 }
