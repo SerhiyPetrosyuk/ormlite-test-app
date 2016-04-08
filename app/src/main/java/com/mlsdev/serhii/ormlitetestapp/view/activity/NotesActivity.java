9e8add4efc3ad10cfbd852bc5767f79b27b7a40b
Serhii Petrosiuk <petrosyuk@mlsdev.com>
2016-04-08 09:44:36 +0300

    Updated the build system files

--- app/src/main/java/com/mlsdev/serhii/ormlitetestapp/view/activity/NotesActivity.java
+++ app/src/main/java/com/mlsdev/serhii/ormlitetestapp/view/activity/NotesActivity.java
@@ -3,6 +3,7 @@ package com.mlsdev.serhii.ormlitetestapp.view.activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.mlsdev.serhii.ormlitetestapp.R;
 import com.mlsdev.serhii.ormlitetestapp.databinding.ActivityNotesBinding;
@@ -14,5 +15,12 @@ public class NotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_notes);
+        initNotesRecyclerView();
     }
+
+    private void initNotesRecyclerView() {
+        binding.rvNotes.setHasFixedSize(true);
+        binding.rvNotes.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
